cba939cba02fd4cd89b46aaf31b51e344638426a
duknow333 <deekshith6675@gmail.com>
2026-04-09 21:47:22 +0530

    neww

---
+++ script.js
@@ -0,0 +1,18 @@
+// Popup
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+// Register
+function register() {
+    alert("Registered Successfully!");
+    closePopup();
+}
+
+
+
+
